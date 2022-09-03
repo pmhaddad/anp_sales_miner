a27ee1f21b4608e88ce5490b274abf78fb3c5376
Paulo Haddad Martim <11465075+pmhaddad@users.noreply.github.com>
2022-09-03 09:20:54 -0300

    Add tests for io_helper

---
+++ tests/helpers/io_helper_test.py
@@ -0,0 +1,61 @@
+import os
+
+import pandas as pd
+import pytest
+
+from anp_sales_miner.helpers.io_helper import make_path, write_as_parquet
+
+
+@pytest.fixture
+def sample_dataframe():
+    df = pd.DataFrame({'a': [0, 1, 2, 3],
+                       'b': ['x', 'x', 'y', 'z']})
+    df['b'] = df['b'].astype('category')
+    return df
+
+
+def test_make_path(tmp_path):
+    dummy_datalake = tmp_path / 'datalake'
+    dummy_datalake.mkdir()
+    os.environ['DATALAKE_PATH'] = str(dummy_datalake)
+
+    path = make_path(stage='a_dummy_stage', table='a_table_name')
+
+    assert path == f'{tmp_path}/datalake/a_dummy_stage/a_table_name'
+
+
+def test_make_path_with_parents_error(tmp_path):
+    os.environ['DATALAKE_PATH'] = 'with/parents/datalake'
+
+    message = r"\[Errno 2\] No such file or directory: 'with/parents/datalake/a_dummy_stage'"
+    with pytest.raises(FileNotFoundError, match=message):
+        make_path(stage='a_dummy_stage', table='a_table_name')
+
+
+def test_make_path_exists_ok_no_error(tmp_path):
+    dummy_datalake = tmp_path / 'datalake'
+    dummy_datalake.mkdir()
+    os.environ['DATALAKE_PATH'] = str(dummy_datalake)
+
+    path = make_path(stage='a_dummy_stage', table='a_table_name')
+    path = make_path(stage='a_dummy_stage', table='a_table_name')
+
+    assert path == f'{tmp_path}/datalake/a_dummy_stage/a_table_name'
+
+
+def test_write_as_parquet(tmp_path, sample_dataframe):
+    output_path = tmp_path / 'df'
+    write_as_parquet(sample_dataframe, output_path=output_path, partitioning=['b'])
+    assert sample_dataframe.equals(pd.read_parquet(output_path))
+
+
+def test_write_as_parquet_overwrite_or_ignore(tmp_path, sample_dataframe):
+    output_path = tmp_path / 'df'
+    write_as_parquet(sample_dataframe, output_path=output_path, partitioning=['b'])
+    assert sample_dataframe.equals(pd.read_parquet(output_path))
+
+    changed_z_partition = pd.DataFrame({'a': [999], 'b': ['z']})
+    write_as_parquet(changed_z_partition, output_path=output_path, partitioning=['b'])
+    expected_df = sample_dataframe.copy()
+    expected_df.loc[3, 'a'] = 999
+    assert expected_df.equals(pd.read_parquet(output_path))
