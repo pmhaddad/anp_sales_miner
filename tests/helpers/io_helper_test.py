036bfd9a51947042d7ba28f257a18eaf6c4f6c59
Paulo Haddad Martim <11465075+pmhaddad@users.noreply.github.com>
2022-09-03 14:52:22 -0300

    Use monkeypatch to make env var modification more robust

--- tests/helpers/io_helper_test.py
+++ tests/helpers/io_helper_test.py
@@ -7,6 +7,13 @@ from anp_sales_miner.helpers.io_helper import make_path, write_as_parquet
 
 
 @pytest.fixture
+def mock_datalake_path(monkeypatch, tmp_path):
+    test_datalake = tmp_path / 'datalake'
+    test_datalake.mkdir()
+    monkeypatch.setenv('DATALAKE_PATH', str(test_datalake))
+
+
+@pytest.fixture
 def sample_dataframe():
     df = pd.DataFrame({'a': [0, 1, 2, 3],
                        'b': ['x', 'x', 'y', 'z']})
@@ -14,33 +21,23 @@ def sample_dataframe():
     return df
 
 
-def test_make_path(tmp_path):
-    dummy_datalake = tmp_path / 'datalake'
-    dummy_datalake.mkdir()
-    os.environ['DATALAKE_PATH'] = str(dummy_datalake)
-
+def test_make_path(mock_datalake_path):
     path = make_path(stage='a_dummy_stage', table='a_table_name')
+    assert path == f'{os.getenv("DATALAKE_PATH")}/a_dummy_stage/a_table_name'
 
-    assert path == f'{tmp_path}/datalake/a_dummy_stage/a_table_name'
 
-
-def test_make_path_with_parents_error(tmp_path):
-    os.environ['DATALAKE_PATH'] = 'with/parents/datalake'
+def test_make_path_with_parents_error(monkeypatch):
+    monkeypatch.setenv('DATALAKE_PATH', 'with/parents/datalake')
 
     message = r"\[Errno 2\] No such file or directory: 'with/parents/datalake/a_dummy_stage'"
     with pytest.raises(FileNotFoundError, match=message):
         make_path(stage='a_dummy_stage', table='a_table_name')
 
 
-def test_make_path_exists_ok_no_error(tmp_path):
-    dummy_datalake = tmp_path / 'datalake'
-    dummy_datalake.mkdir()
-    os.environ['DATALAKE_PATH'] = str(dummy_datalake)
-
+def test_make_path_exists_ok_no_error(mock_datalake_path):
     path = make_path(stage='a_dummy_stage', table='a_table_name')
     path = make_path(stage='a_dummy_stage', table='a_table_name')
-
-    assert path == f'{tmp_path}/datalake/a_dummy_stage/a_table_name'
+    assert path == f'{os.getenv("DATALAKE_PATH")}/a_dummy_stage/a_table_name'
 
 
 def test_write_as_parquet(tmp_path, sample_dataframe):
